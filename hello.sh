b284a4a5ac7df189c950bc196a345efd7b118ba4
guwenbo <bobo1314love@vip.qq.com>
2017-10-23 17:26:10 +0800

    first standard script

---
+++ hello.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# 
+# This script will print 'Hello world' in your screen
+#
+# Author : gin
+# Mail   : bobo1314love@vip.qq.com
+# Build on 2017/10/23 17:20
+# Update on : 2017/10/23 17:20
+
+PATH=/bin:/sbin:/usr/bin:/usr/local/bin
+export PATH
+
+echo -e 'Hello world!'
+exit 0
